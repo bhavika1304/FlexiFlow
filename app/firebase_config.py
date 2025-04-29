8f7f1dfb952185b7b43af6b2c1672e144f9dd68f
Bhavika Gandham <149353470+bhavika1304@users.noreply.github.com>
2025-04-30 03:51:34 +0530

    Update firebase_config.py

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -2,10 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH", "firebase_key.json")
+FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH", "firebase_config.py")
 
 # Load credentials
-cred = credentials.Certificate("config/firebase_key.json")
+cred = credentials.Certificate("firebase_config.py")
 firebase_admin.initialize_app(cred)
 
 # Firestore client
