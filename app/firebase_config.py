422ecbe8ec16b55079244f7aa9204ee7c0d770e9
Bhavika Gandham <149353470+bhavika1304@users.noreply.github.com>
2025-04-30 03:38:26 +0530

    Update firebase_config.py

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,5 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+
+FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH", "firebase_key.json")
 
 # Load credentials
 cred = credentials.Certificate("config/firebase_key.json")
